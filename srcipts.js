b9332471f0b4ae2e2e407dc9d58aeb2f7bcd88c6
nsishuba <nalisishuba@gmail.com>
2023-04-08 22:08:06 +0200

    Changed key values in object tax.

---
+++ srcipts.js
@@ -0,0 +1,37 @@
+const salary = 4000;
+const lodging = 'apartment'
+const si'ze = 'large'
+
+
+// Only change the syntax below (not the values or key names)
+
+const expenses = {
+    food: 51.7501,
+    transport:  10.2,
+}
+  
+const tax = {
+    "734": 0.03,
+    "234": 0.20,
+    "913": 0.12,
+    "415": 0.38,
+    "502": 0.42,
+}
+
+const rent = {
+    none: 0,
+    'small-room': 200,
+    'large-room': 300,
+    'small-apartment': 400,
+    'large-apartment': 800,
+    'small-house': 1200,
+    'large-house': 2400,
+}
+
+// You can change below however you want
+
+const taxAsDecimal = tax.913 / 100
+const startingAfterTax = salary * 1 - taxAsDecimal
+const type = lodging + size
+const balance = expenses(transport) - expenses(food) - expenses(rent.type) 
+console.log(balance)
